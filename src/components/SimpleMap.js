192d90a7e93d7d20a9f0c43fc53619bbde145d32
Mitchell Rathgeb <megafrogg@gmail.com>
2016-10-25 21:39:01 -0400

    Fixed bug in SimpleMap causing JS to break

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -4,8 +4,8 @@ import { GoogleMap, GoogleMapLoader } from 'react-google-maps';
 let mapRef;
 
 const SimpleMap = ({lat, long, markers, onMapClick}) => {
-  if (mapRef) {
-    mapRef.setCenter({lat: lat, lng: long});
+  if (mapRef && mapRef.props && mapRef.props.map) {
+    mapRef.props.map.setCenter({lat: lat, lng: long});
   }
   return (
   <section style={{
@@ -23,7 +23,7 @@ const SimpleMap = ({lat, long, markers, onMapClick}) => {
       }
       googleMapElement={
         <GoogleMap
-          ref={(map) => mapRef = map.props.map}
+          ref={(map) => mapRef = map}
           defaultZoom={10}
           defaultCenter={{ lat: lat, lng: long }}
           onClick={onMapClick}
